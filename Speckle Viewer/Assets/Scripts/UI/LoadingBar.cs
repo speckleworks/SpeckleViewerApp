623de272cbd2e884d366ed48f2b69fa0e37db37d
Jakir Hussain <jakir.hussain@hotmail.co.uk>
2020-04-23 19:16:22 +0100

    Referencing a local version of speckleunity to help with development iteration speeds between this project and speckleunity. Made a basic login screen.

---
+++ Speckle Viewer/Assets/Scripts/UI/LoadingBar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SpeckleUnity;
+
+public class LoadingBar : MonoBehaviour
+{
+    public Image progressBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        progressBar.fillAmount = 0;
+        gameObject.SetActive (false);
+    }
+
+    public void UpdateProgress (SpeckleUnityUpdate updateData)
+    {
+        if (updateData.updateProgress == 0) gameObject.SetActive (true);
+
+        if (updateData.updateProgress == 1) gameObject.SetActive (false);
+
+        progressBar.fillAmount = updateData.updateProgress;
+    }
+}
---
+++ Speckle Viewer/Assets/Scripts/UI/LoginBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using SpeckleUnity;
+using SpeckleCore;
+
+public class LoginBehaviour : MonoBehaviour
+{
+    private bool serverFieldHasInput = true;
+    private bool emailFieldHasInput = false;
+    private bool passwordFieldHasInput = false;
+
+    private string serverURL = "https://hestia.speckle.works/api/";
+    private string email = "";
+    private string password = "";
+
+    public TMP_InputField serverInput;
+    public TMP_InputField emailInput;
+    public TMP_InputField passwordInput;
+
+    public SpeckleUnityManager manager;
+    public Button loginButton;
+
+    public void OnServerFieldInput (string input)
+    {
+        serverFieldHasInput = input.Length > 0;
+        serverURL = input;
+        UpdateLoginButtonState ();
+    }
+
+    public void OnEmailFieldInput (string input)
+    {
+        emailFieldHasInput = input.Length > 0;
+        email = input;
+        UpdateLoginButtonState ();
+    }
+
+    public void OnPasswordFieldInput (string input)
+    {
+        passwordFieldHasInput = input.Length > 0;
+        password = input;
+        UpdateLoginButtonState ();
+    }
+
+    private void UpdateLoginButtonState ()
+    {
+        loginButton.interactable = serverFieldHasInput && emailFieldHasInput && passwordFieldHasInput;
+    }
+
+    public async void AttemptLogin ()
+    {
+        manager.SetServerUrl (serverURL);
+        Debug.Log (email);
+        Debug.Log (password);
+        await manager.LoginAsync (email, password, LoginCallback);
+    }
+
+    private void LoginCallback (User loggedInUser)
+    {
+        gameObject.SetActive (false);
+    }
+}
